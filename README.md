bc728e15bc74b7af1f2e6257727b4b34a685a905
Cristhian Monasterio <cristhian.monasterio@gmail.com>
2025-06-16 12:36:21 -0400

    add gunicorn

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # gunicorn
+
+# Run only werkzeug
+
+```bash
+python app.py
+```
+
+### Run load test
+
+```bash
+./loadtest.sh send_basic_requests 10 1
+```
+
+## Run only gunicorn
+
+```bash
+gunicorn app:app --bind 0.0.0.0:5000 --workers 2 --threads 2 --timeout 120
+```
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from werkzeug.wrappers import Request, Response
 from werkzeug.serving import run_simple
 
 # Define a simple WSGI application
-def application(environ, start_response):
+def app(environ, start_response):
     request = Request(environ)
     text = f"Hello! You made a {request.method} request to {request.path}"
     response = Response(text, content_type='text/plain')
@@ -10,4 +10,4 @@ def application(environ, start_response):
 
 # Run the server on localhost:5000
 if __name__ == '__main__':
-    run_simple('localhost', 5000, application)
+    run_simple('localhost', 5000, app)
